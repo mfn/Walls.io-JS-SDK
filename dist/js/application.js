381cbc0250c851f3a0c86a3b43165827ed29f942
catearcher <indieboy86@gmail.com>
2014-07-08 01:52:14 +0200

    updated example app

--- dist/js/application.js
+++ dist/js/application.js
@@ -4,6 +4,7 @@
       wallStreamOptions: {
         accessToken: "5f864451221b0e8d2ff61b3179ac1a3b5d4ac9e3"
       },
+      template: $("#my-template").html(),
       beforeInsert: function(html, post) {
         return html;
       }
--- src/assets/javascripts/application.coffee
+++ src/assets/javascripts/application.coffee
@@ -1,7 +1,8 @@
 $ ->
-  
+
   $("#mywall").wall
     wallStreamOptions:
       accessToken: "5f864451221b0e8d2ff61b3179ac1a3b5d4ac9e3"
+    template: $("#my-template").html()
     beforeInsert: (html, post) ->
       html
